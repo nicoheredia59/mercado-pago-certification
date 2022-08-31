b95d5af23e04ae84455bc907dbb7a9a414fd32ac
nicoheredia59 <nicoheredia59@gmail.com>
2022-08-31 18:10:01 -0300

    feat: product array item(temporal)

---
+++ assets/products.ts
@@ -0,0 +1,75 @@
+
+export const products = [
+  {
+    id: 1,
+    title: 'Nose',
+    unit_price: 30,
+    quantity: 1,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dolorem quia, ducimus dicta laboriosam rerum eius earum itaque optio? Nostrum aspernatur placeat harum quis, numquam tenetur asperiores in! Ex, pariatur!',
+    picture_url: 'http://www.myapp.com/myimage.jpg',
+    category_id: ''
+  },
+  {
+    id: 2,
+    title: 'Nose',
+    unit_price: 30,
+    quantity: 1,
+    description: 'Dummy description',
+    picture_url: 'http://www.myapp.com/myimage.jpg',
+    category_id: ''
+  },
+  {
+    id: 3,
+    title: 'Nose',
+    unit_price: 30,
+    quantity: 1,
+    description: 'Dummy description',
+    picture_url: 'http://www.myapp.com/myimage.jpg',
+    category_id: ''
+  },
+  {
+    id: 4,
+    title: 'Nose',
+    unit_price: 30,
+    quantity: 1,
+    description: 'Dummy description',
+    picture_url: 'http://www.myapp.com/myimage.jpg',
+    category_id: ''
+  },
+  {
+    id: 5,
+    title: 'Nose',
+    unit_price: 30,
+    quantity: 1,
+    description: 'Dummy description',
+    picture_url: 'http://www.myapp.com/myimage.jpg',
+    category_id: ''
+  },
+  {
+    id: 6,
+    title: 'Nose',
+    unit_price: 30,
+    quantity: 1,
+    description: 'Dummy description',
+    picture_url: 'http://www.myapp.com/myimage.jpg',
+    category_id: ''
+  },
+  {
+    id: 7,
+    title: 'Nose',
+    unit_price: 30,
+    quantity: 1,
+    description: 'Dummy description',
+    picture_url: 'http://www.myapp.com/myimage.jpg',
+    category_id: ''
+  },
+  {
+    id: 8,
+    title: 'Nose',
+    unit_price: 30,
+    quantity: 1,
+    description: 'Dummy description',
+    picture_url: 'http://www.myapp.com/myimage.jpg',
+    category_id: ''
+  }
+]
