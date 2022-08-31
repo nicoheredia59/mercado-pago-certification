b92f4519903961ea4e854563830475251b8aa15d
nicoheredia59 <nicoheredia59@gmail.com>
2022-08-31 18:10:42 -0300

    feat: add layout & swr config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import MainLayout from '../components/layout/main'
+import { SWRConfig } from 'swr'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }: AppProps) {
+  return (
+    <SWRConfig
+      value={{
+        refreshInterval: 3000,
+        fetcher: async (resource, init) => await fetch(resource, init).then(async res => await res.json())
+      }}
+    >
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </SWRConfig>
+  )
 }
 
 export default MyApp
